2da79021504d5a7a91d35e446caa90843f1e8f55
yeongjun Kim <kyjun519@gmail.com>
2023-08-23 03:59:44 +0900

    feat: Header 컴포넌트 구현

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+type TagType = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+
+export interface HeaderProps {
+  children: React.ReactNode;
+  level: number;
+  strong: boolean;
+  underline: boolean;
+  color: string;
+  style?: React.CSSProperties;
+}
+
+const Header = ({ children, level = 1, strong, underline, color, ...props }: HeaderProps) => {
+  let Tag = `h${level}` as TagType;
+  if (level < 1 || level > 6) {
+    console.warn("Header only accept `1 | 2 | 3 | 4 | 5 | 6` as `level` value.");
+    Tag = "h1";
+  }
+
+  const fontStyle = {
+    fontWeight: strong ? "bold" : "normal",
+    textDecoration: underline ? "underline" : undefined,
+    color,
+  };
+
+  return (
+    <Tag style={{ ...props.style, ...fontStyle }} {...props}>
+      {children}
+    </Tag>
+  );
+};
+
+export default Header;
---
+++ src/stories/Header.stories.tsx
@@ -0,0 +1,23 @@
+import Header from "../components/Header";
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta = {
+  title: "Component/Header",
+  component: Header,
+  args: {
+    level: 1,
+    strong: false,
+    underline: false,
+    color: "red",
+  },
+  argTypes: {
+    level: { control: { type: "range", min: 1, max: 6 } },
+  },
+} satisfies Meta<typeof Header>;
+
+export default meta;
+type Story = StoryObj<typeof Header>;
+
+export const Default: Story = {
+  render: (args) => <Header {...args}>Header</Header>,
+};
