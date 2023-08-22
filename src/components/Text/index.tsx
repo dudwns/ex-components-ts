6dbf9a9eb73a7a04eb19345fc2ac55bd7e2acaa5
yeongjun Kim <kyjun519@gmail.com>
2023-08-23 02:59:16 +0900

    feat: Text 컴포넌트 구현

---
+++ src/components/Text/index.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import "./Text.css";
+
+export interface TextProps {
+  children: React.ReactNode;
+  size: number | string;
+  block: boolean;
+  paragraph: boolean;
+  delete: boolean;
+  code: boolean;
+  mark: boolean;
+  strong: boolean;
+  color: string;
+  underline: boolean;
+  style?: React.CSSProperties;
+}
+
+const Text = ({
+  children,
+  block,
+  paragraph,
+  size,
+  strong,
+  underline,
+  delete: del,
+  color,
+  mark,
+  code,
+  ...props
+}: TextProps) => {
+  const Tag = block ? "div" : paragraph ? "p" : "span";
+  const fontStyle = {
+    fontWeight: strong ? "bold" : undefined,
+    fontSize: typeof size === "number" ? size : undefined,
+    textDecoration: underline ? "underline" : undefined,
+    color,
+  };
+
+  if (mark) {
+    children = <mark>{children}</mark>;
+  }
+  if (code) {
+    children = <code>{children}</code>;
+  }
+  if (del) {
+    children = <del>{children}</del>;
+  }
+
+  return (
+    <Tag
+      className={typeof size === "string" ? `Text--size-${size}` : undefined}
+      style={{ ...props.style, ...fontStyle }}
+      {...props}
+    >
+      {children}
+    </Tag>
+  );
+};
+
+export default Text;
---
+++ src/stories/Text.stories.tsx
@@ -0,0 +1,40 @@
+import Text from "../components/Text";
+import { Meta, StoryObj } from "@storybook/react";
+
+const meta = {
+  title: "Component/Text",
+  component: Text,
+  args: {
+    size: 20,
+    strong: false,
+    underline: false,
+    delete: false,
+    color: "red",
+    block: false,
+    paragraph: false,
+    mark: false,
+    code: false,
+    children: <div>Custom</div>,
+  },
+  argTypes: {},
+} satisfies Meta<typeof Text>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  render: (args) => (
+    <>
+      <Text {...args} size="large">
+        large
+      </Text>
+      <Text {...args} size="normal">
+        normal
+      </Text>
+      <Text {...args} size="small">
+        small
+      </Text>
+      <Text {...args} size={24} />
+    </>
+  ),
+};
