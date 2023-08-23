8fccafb16945c04a85b1e097a103b6e1b5736b78
yeongjun Kim <kyjun519@gmail.com>
2023-08-23 16:01:56 +0900

    feat: Toggle 컴포넌트 구현

---
+++ src/stories/Toggle.stories.tsx
@@ -0,0 +1,17 @@
+import Toggle from "../components/Toggle";
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta = {
+  title: "Component/Toggle",
+  component: Toggle,
+  args: {
+    disabled: false,
+  },
+} satisfies Meta<typeof Toggle>;
+
+export default meta;
+type Story = StoryObj<typeof Toggle>;
+
+export const Default: Story = {
+  render: (args) => <Toggle {...args} />,
+};
